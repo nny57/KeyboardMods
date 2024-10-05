323dc3537719c4ccf2d1fae409df98dbf9a6e259
Narendran Narayanasamy <59618614+nny57@users.noreply.github.com>
2024-10-05 11:35:44 -0500

    Added lshift behaviour

--- hammerspoon-Init.lua
+++ hammerspoon-Init.lua
@@ -148,3 +148,155 @@ hs.hotkey.bind({"cmd"}, "q", function()
 end)
 
   
+---------------------------------------------------------------------------- 
+-- lshift changs
+--------------------------------------------------------------------------
+-- Flag to control alert visibility
+local showAlerts = false
+
+-- Initialize state variables
+local leftShiftAsModifier = false
+local stateTimer = nil
+local sentHyperJ = false
+
+-- Function to show alerts if enabled
+local function showAlert(message)
+    if showAlerts then
+        hs.alert.show(message, 2)
+    end
+end
+
+-- Function to send Hyper+J keystroke
+local function sendHyperJ()
+    hs.eventtap.keyStroke(hyper, "j")
+    showAlert("Sent Hyper+J")
+    sentHyperJ = true  -- Mark that we've sent Hyper+J
+end
+
+-- Function to reset all state variables
+local function resetState()
+    if stateTimer then
+        stateTimer:stop()
+        stateTimer = nil
+    end
+    leftShiftAsModifier = false
+    sentHyperJ = false
+end
+
+-- Create an event tap to monitor keyboard events
+leftShiftTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, function(event)
+    local flags = event:getFlags()
+    local keyCode = event:getKeyCode()
+    local eventType = event:getType()
+    
+    if keyCode == 56 then  -- Left Shift key code
+        if eventType == hs.eventtap.event.types.flagsChanged then
+            if flags.shift then
+                -- Left Shift pressed
+                showAlert("Left Shift Pressed")
+                resetState()
+                -- Start a timer to detect if Left Shift is held
+                stateTimer = hs.timer.doAfter(0.15, function()
+                    showAlert("Left Shift Hold Detected")
+                    resetState()
+                end)
+            else
+                -- Left Shift released
+                showAlert("Left Shift Released")
+                if stateTimer then
+                    stateTimer:stop()
+                    stateTimer = nil
+                    if not leftShiftAsModifier then
+                        -- If Left Shift wasn't used as a modifier, send Hyper+J
+                        sendHyperJ()
+                        resetState()
+                        return true  -- Consume the event
+                    end
+                end
+                resetState()
+            end
+        end
+    elseif eventType == hs.eventtap.event.types.keyDown then
+        if flags.shift and not sentHyperJ then
+            -- Another key pressed while Left Shift is down and we haven't just sent Hyper+J
+            leftShiftAsModifier = true
+            if stateTimer then
+                stateTimer:stop()
+                stateTimer = nil
+            end
+            showAlert("Left Shift + " .. hs.keycodes.map[keyCode])
+        end
+    elseif eventType == hs.eventtap.event.types.keyUp then
+        if keyCode == 38 and sentHyperJ then  -- 38 is the key code for 'J'
+            -- Consume the keyUp event for 'J' if we've sent Hyper+J
+            sentHyperJ = false
+            return true
+        end
+    end
+    
+    return false  -- Don't consume the event by default
+end)
+
+-- Start the event tap
+leftShiftTap:start()
+
+
+
+
+
+-- --------------------------------------------------------------------------
+-- -- Show all key press
+-- --------------------------------------------------------------------------
+
+
+-- -- Table to store currently pressed keys
+-- local pressedKeys = {}
+
+-- -- Function to get a string representation of pressed keys
+-- local function getPressedKeysString()
+--     local keyStrings = {}
+--     for key, _ in pairs(pressedKeys) do
+--         table.insert(keyStrings, key)
+--     end
+--     return table.concat(keyStrings, " + ")
+-- end
+
+-- -- Create an event tap to capture all key events
+-- keyTap = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp, hs.eventtap.event.types.flagsChanged}, function(event)
+--     local type = event:getType()
+--     local keyInfo = ""
+--     local flags = event:getFlags()
+
+--     -- Update modifier keys
+--     for _, modifier in ipairs({"fn", "cmd", "ctrl", "alt", "shift"}) do
+--         if flags[modifier] then
+--             pressedKeys[modifier] = true
+--         else
+--             pressedKeys[modifier] = nil
+--         end
+--     end
+
+--     if type == hs.eventtap.event.types.keyDown then
+--         -- Regular key press
+--         local keyCode = event:getKeyCode()
+--         local keyName = hs.keycodes.map[keyCode]
+--         pressedKeys[keyName or string.format("Key(%d)", keyCode)] = true
+--     elseif type == hs.eventtap.event.types.keyUp then
+--         -- Key release
+--         local keyCode = event:getKeyCode()
+--         local keyName = hs.keycodes.map[keyCode]
+--         pressedKeys[keyName or string.format("Key(%d)", keyCode)] = nil
+--     end
+
+--     keyInfo = getPressedKeysString()
+
+--     if keyInfo ~= "" then
+--         hs.alert.closeAll()  -- Close any existing alerts
+--         hs.alert.show(keyInfo, {textSize=16})
+--     end
+
+--     return false  -- Allow the event to propagate
+-- end)
+
+-- -- Start capturing key events
+-- keyTap:start()
